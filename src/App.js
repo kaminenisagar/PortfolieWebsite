4183473c79fa6a4971ee644467b01152244edc35
kaminenisagar <134841177+kaminenisagar@users.noreply.github.com>
2025-03-19 12:46:35 +0530

    Update App.js
    Signed-off-by: kaminenisagar <134841177+kaminenisagar@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import { BrowserRouter,Switch,Route } from 'react-router-dom'
 
 import Home from './components/Home'
-import About from './components/Home'
-import Skills from './components/Home'
-import Projects from './components/Home'
-import Contacts from './components/Home'
+
 
 import './App.css';
 
@@ -13,9 +10,7 @@ function App() {
     <BrowserRouter>
       <Switch>
           <Route exact path="/" component={Home}/>          
-          <Route exact path="/skills" component={Skills}/>
-          <Route exact path="/projects" component={Projects}/>
-          <Route exact path="/contact" component={Contacts}/>
+     
       </Switch>
     </BrowserRouter>
   );
